c28a71f4e082c5d1ffd935086304e38bceb06bf5
Deysi Merino <111298838+deysimm@users.noreply.github.com>
2022-09-18 17:06:22 -0600

    Update script.js

--- assignments/assignment4/assignment4-solution-starter/easier/script.js
+++ assignments/assignment4/assignment4-solution-starter/easier/script.js
@@ -63,5 +63,17 @@ for (var i = 0; i < names.length; i++) {
     helloSpeaker.speak(names[i]);
   }
 }
+  
+(function(){
+var names =  ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+for (name in names);
+var firstLetter = names[name].charAt(0);
+if (firstLetter.toLowerCase() == "j"){
+byeSpeaker.speak(names[name]);
+} else {
+helloSpeaker.speak(names[name]);
+}
+  
+}
 
 })();
